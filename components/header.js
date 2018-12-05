ed3362ff9ee41823c98903017d9d65fcfcbbc962
giimando <gerry.hoe@gmail.com>
2018-12-05 21:27:48 +0800

    Creates stateful functional component

--- components/header.js
+++ components/header.js
@@ -7,15 +7,33 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 // This is a stateful functional component
 class Header extends React.Component {
 
-    constructor(props){
-        super(props);
-    this.textIwant = props.textIwant
+    constructor(props){ // Set state up
+        super(props);// Run state
+
+        // How we create properties
+        this.textIwant = props.textIwant;
+        // How we create state
+        this.state = {
+            number:0 
+        };
+// Boiler plate to write when adding function
+        this.addNumberWhenPressed = this.addNumberWhenPressed.bind(this);
     }
 
-    
-    render(){
-        return (
-            <Text>{this.textIwant}</Text>
+    componentDidUpdate(){ // any state changed, run the function inside this
+        console.log(this.state.number);
+    }
+   
+addNumberWhenPressed() { //how we manipulate state
+    const previousNumber = this.state.number; //variable that store current state
+    this.setState({ // function to change the state
+        number: previousNumber + 1
+    });
+}
+
+    render() {
+        return ( 
+            <Text onPress={this.addNumberWhenPressed}>{this.textIwant}</Text>
         );
     }
 }
