7dc6e7d7782ad271ccdaa13bcc039a4ef0a6a975
giimando <gerry.hoe@gmail.com>
2018-12-10 22:01:37 +0800

    List view added

--- Screen/todo.js
+++ Screen/todo.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 
 class TodoScreen extends React.Component{
     static navigationOptions = {
         title: 'ToDo',
       };
     render(){
-        return( <Text> Todo App  </Text>);
+        return( 
+            <ScrollView>
+                <View style={styles.container1} />
+                <View style={styles.container2} />
+                <View style={styles.container3} />
+                <View style={styles.container4} />
+            </ScrollView>
+        );
     }
 }
 
+const styles = StyleSheet.create({
+    container1: {
+        padding: 100, 
+        backgroundColor: 'black'},
+        container2: {
+            padding: 100, 
+            backgroundColor: 'grey'},   
+            container3: {
+                padding: 100, 
+                backgroundColor: 'white'},
+                container4: {
+                    padding: 100, 
+                    backgroundColor: 'red'},
+    }
+);
 export default TodoScreen;
